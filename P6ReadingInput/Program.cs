77e0c2a6650051aa1f49fa4eef4a2519071a37d2
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-28 20:01:10 +0200

    P6ReadingInput

---
+++ P6ReadingInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P6ReadingInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Hey, What is your name?: ");
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+
+        }
+    }
+}
