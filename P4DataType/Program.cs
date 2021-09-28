df03b7ad92ba7076857acb00fda4d33cc9ce51d4
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-28 17:41:10 +0200

    P4DataType

---
+++ P4DataType/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P4DataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputString;
+            float decimalValue;
+            bool isAwesome;
+            byte someByte;
+
+            decimalValue = 0.3F;
+            isAwesome = true;
+            outputString = "Hello World";
+            someByte = 0xa7;
+            
+            Console.WriteLine(outputString);
+            Console.WriteLine(decimalValue);
+            Console.WriteLine(isAwesome);
+            Console.WriteLine(someByte);
+        }
+    }
+}
