1c0cb3c60d7ceaeef904af7dd9c69174b6fbbb27
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-28 16:44:44 +0200

    P2Comments

---
+++ P2Comments/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P2Comments
+{
+    class Program
+    {
+        static void Main(string[] args) //attach a word to a string
+        {
+            Console.WriteLine("Hello World"); //prints string
+            Console.WriteLine(DateTime.Now); //show the todays date and time 
+            //Emir Ulusoy
+            //2021-09-28  
+            //emir.ulusoy@student.forsbergsskola.se 
+        }
+    }
+}
