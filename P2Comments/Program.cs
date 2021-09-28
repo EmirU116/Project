cdfa2a1c8251ddd6b423a92555ad6be82547646b
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-28 17:07:37 +0200

    P2Comment Update

--- P2Comments/Program.cs
+++ P2Comments/Program.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿//Emir Ulusoy
+//2021-09-28  
+//emir.ulusoy@student.forsbergsskola.se;
+using System;
 
 namespace P2Comments
 {
     class Program
-    {
+    {   
+        /// <summary>
+        /// Main method prints Hello World to the console
+        /// </summary>
+        /// <param name="args">This is Irrelevent</param>
         static void Main(string[] args) //attach a word to a string
         {
-            Console.WriteLine("Hello World"); //prints string
-            Console.WriteLine(DateTime.Now); //show the todays date and time 
-            //Emir Ulusoy
-            //2021-09-28  
-            //emir.ulusoy@student.forsbergsskola.se 
+            //this will print Hello World
+            Console.WriteLine("Hello World");
+
         }
     }
 }
