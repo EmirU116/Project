3136260742eaaba2859892e6e1d7d126d6830370
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-28 19:54:31 +0200

    P5Conversion

---
+++ P5Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace P5Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            string numberString = number.ToString();
+            Console.WriteLine(numberString);
+
+            numberString = 56.3.ToString();
+            double d = Convert.ToDouble(numberString);
+            number = (int) d;
+            Console.WriteLine(number);
+        }
+    }
+}
