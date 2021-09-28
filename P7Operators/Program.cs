c2a0f1b59664a2320acd1d61125bc92c0e73ec07
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-28 21:03:34 +0200

    P7Operator

---
+++ P7Operators/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P7Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(((7+5)*3 - 6) / 1.2 % 17);
+
+        }
+    }
+}
