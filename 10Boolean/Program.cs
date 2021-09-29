fb02a1e194c54b8a52371a42b8bdba1156e31488
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-29 18:08:18 +0200

    P10Boolean Done

---
+++ 10Boolean/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _10Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?: ");
+            bool isChild = true;
+            bool isTeenager = true;
+            bool isAdult = true;
+            
+            int age = Convert.ToInt16(Console.ReadLine());
+            if (isChild && (age < 12))
+            {
+                Console.WriteLine("You are a Child: True ");
+                Console.WriteLine("You are a Teenager: False");
+                Console.WriteLine("You are a Adult: False ");
+            }
+            else if (isTeenager && (age < 19))
+            {
+                Console.WriteLine("You are a Child: False ");
+                Console.WriteLine("You are a Teenager: True");
+                Console.WriteLine("You are a Adult: False "); 
+            } 
+            else if (isAdult && (age >= 20))
+            {
+                Console.WriteLine("You are a Child: False ");
+                Console.WriteLine("You are a Teenager: False");
+                Console.WriteLine("You are a Adult: True "); 
+            } 
+
+        }
+    }
+}
