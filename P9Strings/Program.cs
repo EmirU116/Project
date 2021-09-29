77cd609966e4617559216dffd1545b3db8de5e85
EmirU116 <emir.ulusoy@student.forsbergsskola.se>
2021-09-29 15:12:53 +0200

    P9Strings

---
+++ P9Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P9Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey, What is your first name?: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("What is your last name?: ");
+            string lastname = Console.ReadLine();
+            
+            Console.WriteLine("How old are you?: ");
+            string age = Console.ReadLine();
+            
+            Console.WriteLine("Hello " + name + " " + lastname + ", you are " + age + " years old.");
+        }
+    }
+}
